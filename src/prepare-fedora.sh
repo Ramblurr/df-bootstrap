e78bf57bba1a0f7d6f7329ec81813f3ac88014ef
Casey Link <unnamedrambler@gmail.com>
2013-05-06 10:04:44 +0200

    fedora: update df dependencies

--- src/prepare-fedora.sh
+++ src/prepare-fedora.sh
@@ -13,7 +13,14 @@ sudo yum install libgcc.i686 git cmake glibc-devel.i686 zlib-devel.i686 perl-XML
 sudo yum install mercurial qt.i686 libgcc.i686 qt-devel
 
 # dwarf fortress dependencies
-sudo yum install openal-soft.i686 libsndfile.i686
+sudo yum install \
+    SDL.i686 \
+    SDL_image.i686 \
+    SDL_ttf.i686 \
+    gtk2.i686 \
+    mesa-libGLU.i686 \
+    openal-soft.i686 \
+    libsndfile.i686
 
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "fedora" > $SRC_DIR/distro
